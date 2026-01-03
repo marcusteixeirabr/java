4b3a7c7adb3b54be5a52e566a9c886912ea99e95
marcusteixeirabr <marcusteixeirabr@gmail.com>
2026-01-03 20:40:07 +0000

    Adiciona código para estudo da anotação @Value no projeto primeiros-passos.

--- primeiros-passos/src/main/java/dio/bootcamp/springboot/primeiros_passos/app/SistemaMensagem.java
+++ primeiros-passos/src/main/java/dio/bootcamp/springboot/primeiros_passos/app/SistemaMensagem.java
@@ -1,14 +1,25 @@
 package dio.bootcamp.springboot.primeiros_passos.app;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SistemaMensagem {
+public class SistemaMensagem implements CommandLineRunner {
     @Autowired
     private Remetente noreply;
     @Autowired
     private Remetente techTeam;
+
+    @Value("${nome:NoReply}")
+    private String nome;
+    @Value("${email}")
+    private String email;
+    @Value("${telefones}")
+    private List<Long> telefones;
 
     public void enviarConfirmacaoCadastro() {
         System.out.println(noreply);
@@ -20,4 +31,12 @@ public class SistemaMensagem {
         System.out.println(techTeam);
         System.out.println("Seja bem-vindo(a) a Tech Elite ...");
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Mensagem enviada por: " + nome + "\nE-mail: " + email + "\nCom telefones para contato: " + telefones);
+        System.out.println("Seu cadastro foi aprovado ...");
+    }
+
+    
 }
