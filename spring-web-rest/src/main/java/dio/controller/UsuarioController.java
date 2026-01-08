032688a6f229f0473af1a6ca39220de801018860
marcusteixeirabr <marcusteixeirabr@gmail.com>
2026-01-08 01:47:07 +0000

    Sunstitui a dependência springfox Swagger 3.0.0 por SpringDoc OpenAPI 2.7.0 no pom.xml, corrige o SwaggerConfig para usar o springdoc e atualiza os endpoints de documentação Swagger para refletir as mudanças na configuração.

--- spring-web-rest/src/main/java/dio/controller/UsuarioController.java
+++ spring-web-rest/src/main/java/dio/controller/UsuarioController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -42,6 +44,11 @@ public class UsuarioController {
 
     @PostMapping()
     public void postUsuario(@RequestBody Usuario usuario) {
+        repository.save(usuario);
+    }
+
+    @PutMapping()
+    public void putUsuario(@RequestBody Usuario usuario) {
         repository.save(usuario);
     }
 
