da9f1edf7f32592f60640d223ef38fe046f38b25
marcusteixeirabr <marcusteixeirabr@gmail.com>
2026-01-15 02:05:50 +0000

    Adiciona o projeto movimentacao-navios ao repositorio para controle da movimentação de navios no porto de Itajaí

---
+++ movimentacao-navios/src/test/java/mb/psc/MovimentacaoNaviosApplicationTests.java
@@ -0,0 +1,13 @@
+package mb.psc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovimentacaoNaviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
